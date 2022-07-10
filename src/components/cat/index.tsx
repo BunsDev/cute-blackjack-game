ae3c672c4b4b60deacd5057983bcd94366f12e69
Buns <68980965+BunsDev@users.noreply.github.com>
2022-07-10 08:02:21 -0500

    i cant stop
    i wont stop

--- src/components/cat/index.tsx
+++ src/components/cat/index.tsx
@@ -2,8 +2,8 @@ import { styled } from '@stitches/react';
 import catAvatar from './catArt.png';
 
 const CatAvatar = styled('div', {
-  width: '400px',
-  height: '400px',
+  width: '300px',
+  height: '300px',
 
   background: `url(${catAvatar})`,
 });
